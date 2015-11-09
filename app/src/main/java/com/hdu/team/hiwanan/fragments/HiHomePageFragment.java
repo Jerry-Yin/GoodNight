e45cccefebc9c5dcf656321ae03e71279ebf7f1b
JerryYin <846597629@qq.com>
2015-11-09 10:54:28 +0800

    添加了主页面的add按钮

--- app/src/main/java/com/hdu/team/hiwanan/fragments/HiHomePageFragment.java
+++ app/src/main/java/com/hdu/team/hiwanan/fragments/HiHomePageFragment.java
@@ -1,41 +1,68 @@
 package com.hdu.team.hiwanan.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.hdu.team.hiwanan.R;
 
 /**
  * Created by JerryYin on 11/3/15.
  */
-public class HiHomePageFragment extends Fragment implements ViewPager.OnPageChangeListener {
+public class HiHomePageFragment extends Fragment implements View.OnClickListener {
 
+    /**Constants*/
+    private View mContentView;
+    private Activity mSelf;
 
+    /**View*/
+    private ImageButton mbtnAddClock;
+
+    public Activity getmSelf(){
+        return mSelf;
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.layout_home_page, container, false);
+        if (null != mContentView){
+            ViewGroup vg = (ViewGroup) mContentView.getParent();
+            if (null != vg){
+                vg.removeView(mContentView);
+            }
+        }else {
+            mContentView = inflater.inflate(R.layout.layout_home_page, container, false);
+            mSelf = getmSelf();
+            setupViews();
+        }
+        return mContentView;
     }
+
+
+    private void setupViews() {
+        mbtnAddClock = (ImageButton) mContentView.findViewById(R.id.btn_add_clock);
+        mbtnAddClock.setOnClickListener(this);
+
+    }
+
 
     @Override
-    public void onPageScrolled(int i, float v, int i1) {
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_add_clock:
 
+                break;
+
+            default:
+                break;
+        }
     }
 
-    @Override
-    public void onPageSelected(int i) {
-
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int i) {
-
-    }
 }
